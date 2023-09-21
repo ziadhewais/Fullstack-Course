f4c9b2bf535282f2b7f0c0c2448b6d52622b7a4a
ziadhewais <zewais@sl.on.ca>
2023-09-21 12:17:58 -0400

    Startof cards for diamod world

---
+++ first-react-app/src/Card.jsx
@@ -0,0 +1,10 @@
+// import diamond1 from "./assets/pexels-the-glorious-studio-10475791.jpg"
+export default function Card(){
+    return(
+        <div className="card">
+            <img src="src\assets\pexels-the-glorious-studio-10475791.jpg" alt="" width="100"/>
+            <h2>Ice Lake</h2>
+            <p>$899</p>
+        </div>
+    )
+}
